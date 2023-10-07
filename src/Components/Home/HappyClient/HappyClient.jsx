4579f0b101337c58da141f3e5b07666aacf6fc72
naimur8606 <119111473+naimur8606@users.noreply.github.com>
2023-10-07 14:41:14 +0600

    'cliend say' section added

--- src/Components/Home/HappyClient/HappyClient.jsx
+++ src/Components/Home/HappyClient/HappyClient.jsx
@@ -5,13 +5,15 @@ const HappyClient = () => {
     const { clientSay } = useContext(AuthContext);
     console.log(clientSay)
     return (
-        <div>
+        <div className="w-11/12 mx-auto my-5">
             <h2 className="text-2xl md:text-3xl lg:text-5xl font-semibold text-center my-10 text-[#fd4520]"> Client Say </h2>
-            <div>
+            <div className="grid md:grid-cols-3 gap-5 text-center">
                 {
                     clientSay?.map(client =>
-                        <div key={client?.id}>
-                            <img src="" alt="" />
+                        <div key={client?.id} className="border px-2 py-8 space-y-2 shadow-md rounded-lg">
+                            <img className="rounded-[50%] h-36 w-36 mx-auto" src={client?.image} alt="" />
+                            <h3 className="text-xl font-medium">Name: {client?.name} {`(${client?.title})`}</h3>
+                            <p className="text-[#848199]">{client?.quote}</p>
                         </div>
                     )
                 }
